eebf501468c2e654bb0aa1b296b6c0b8c3de72d9
Morten Gjerding <mortengjerding@gmail.com>
2019-09-12 08:46:21 +0200

    Update with setup.py

--- qeh/__init__.py
+++ qeh/__init__.py
@@ -1,6 +1,2 @@
-from gpaw.qeh.qeh import (Heterostructure,
-                          BuildingBlock,
-                          interpolate_building_blocks,
-                          make_heterostructure,
-                          plot_plasmons,
-                          expand_layers)
+__version__ = '0.1'
+name = "qeh"
