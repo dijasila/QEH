00068f017c3db1bd66dd079cbf104296956ef644
Fredrik Andreas Nilsson <fanni@DTU-FVFGC14Q05G.lan>
2022-11-30 10:30:04 +0100

    automatically adjust edgesize

--- qeh/qeh.py
+++ qeh/qeh.py
@@ -269,18 +269,16 @@ class Heterostructure:
 
         # Grid stuff
         edgesize = 50
+        dsum = np.sum(self.d)
         if self.substrate is not None: #F.N
-            edgesize *= 2.
-            edgesize += np.sum(self.d)/2.
-        system_size = np.sum(self.d) + edgesize
+            edgesize += dsum
+        system_size = dsum + edgesize
         self.poisson_lim = 1000  # above this limit use potential model
         assert system_size < self.poisson_lim
 
         self.z_lim = system_size
         self.dz = 0.05
         # master grid
-        #if self.substrate is not None: #F.N
-        #    edgesize+=np.sum(self.d)
         self.z_big = np.arange(0, self.z_lim, self.dz)
         self.z_big -= self.z_big[-1]/2.0
         self.z0 = np.append(np.array([0]), np.cumsum(self.d))
